9c4eba8bb8306ba70bea960cc9ac0a4de49e8212
TAMAGO55123 <tamago.55123@gmail.com>
2025-08-03 12:37:31 +0900

    行数制限、エラー追加

--- cogs/sc_ex.py
+++ cogs/sc_ex.py
@@ -14,38 +14,46 @@ class ScExCog(commands.Cog):
     async def on_ready(self):
         bot_ready_print("SCEXCog")
     
-    async def scratch_expand(self, channel_id:int, id:int):
+    async def scratch_expand(self, channel_id:int, id:int, message:discord.Message):
         async with aiohttp.ClientSession() as session:
             async with session.get(f"https://api.scratch.mit.edu/projects/{id}/") as resp:
                 data = await resp.json()
-                # print(data)
-                embeds = []
-                embeds.append(discord.Embed(
-                    title=data["title"],
-                    url=f"https://scratch.mit.edu/projects/{id}/",
-                    color=0xfda747,
-                ))
-                if data["instructions"] != None:
+                print(data)
+                if data["code"] == "NotFound":
+                    await message.reply(embed=discord.Embed(
+                        title="エラー",
+                        description="無効なURLです",
+                        color=0xff0000
+                    ),allowed_mentions=discord.AllowedMentions(users=False, replied_user=False, roles=False,everyone=False))
+                else:
+                    embeds = []
                     embeds.append(discord.Embed(
-                        title="使い方",
-                        description=data["instructions"],
+                        title=data["title"],
+                        url=f"https://scratch.mit.edu/projects/{id}/",
+                        color=0xfda747,
+                    ))
+                    if data["instructions"] != None:
+                        
+                        embeds.append(discord.Embed(
+                            title="使い方",
+                            description=(len(data["instructions"]) if len(data["instructions"]) < 15 else "\n".join(re.split("\n",data["instructions"])[:5])),
+                            color=0xfda747
+                        ))
+                    if data["description"] != None:
+                        embeds.append(discord.Embed(
+                            title="メモとクレジット",
+                            description=(len(data["description"]) if len(data["description"]) < 15 else "\n".join(re.split("\n",data["description"])[:5])),
+                            color=0xfda747
+                        ))
+                    embeds.append(discord.Embed(
+                        title="ステータス",
+                        description=f"<:love:1401290862956253324> : {data['stats']['loves']} | <:fav:1401290849857310800> : {data['stats']['favorites']} | <:remix:1401290874784190484> : {data['stats']['remixes']} | <:views:1401290886205411560> : {data['stats']['views']}",
                         color=0xfda747
                     ))
-                if data["description"] != None:
-                    embeds.append(discord.Embed(
-                        title="メモとクレジット",
-                        description=data["description"],
-                        color=0xfda747
-                    ))
-                embeds.append(discord.Embed(
-                    title="ステータス",
-                    description=f"<:love:1401290862956253324> : {data['stats']['loves']} | <:fav:1401290849857310800> : {data['stats']['favorites']} | <:remix:1401290874784190484> : {data['stats']['remixes']} | <:views:1401290886205411560> : {data['stats']['views']}",
-                    color=0xfda747
-                ))
-                embeds[0].set_author(name=data["author"]["username"],icon_url=data["author"]["profile"]["images"]["90x90"])
-                embeds[0].set_thumbnail(url=data["image"])
+                    embeds[0].set_author(name=data["author"]["username"],icon_url=data["author"]["profile"]["images"]["90x90"])
+                    embeds[0].set_thumbnail(url=data["image"])
 
-                await self.bot.get_channel(channel_id).send(embeds=embeds)
+                    await self.bot.get_channel(channel_id).send(embeds=embeds)
 
     @commands.Cog.listener()
     async def on_message(self, message:discord.Message):
@@ -62,7 +70,7 @@ class ScExCog(commands.Cog):
                 # print(url_re_id)
                 
                 for i in url_re_id:
-                    await self.scratch_expand(channel_id=channel_id,id=i)
+                    await self.scratch_expand(channel_id=channel_id, id=i, message=message)
     
     class scex1(app_commands.Group):
         pass
